cf59910d54dcc280cb3287add7e88a593776cb5a
Vipul Dessai <badgamerbad@gmail.com>
2022-05-15 19:05:55 +0530

    dsa

---
+++ src/Data structures algorithms/03-Sliding Window/001-best-time-buy-sell-stock.ts
@@ -0,0 +1,19 @@
+function maxProfit(prices: number[]): number {
+  let l = 0,
+    r = 1,
+    maxP = 0;
+
+  while (r <= prices.length - 1) {
+    if (prices[l] < prices[r]) {
+      maxP = Math.max(maxP, prices[r] - prices[l]);
+    } else {
+      l = r;
+    }
+
+    ++r;
+  }
+
+  return maxP;
+}
+
+console.log(maxProfit([1, 2]));
