e0b8ab9ab14a3700f1893c292a2b92b73cf3d079
Vipul Dessai <badgamerbad@gmail.com>
2022-05-22 01:51:49 +0530

    dsa

---
+++ src/Data structures algorithms/07-Trees/04-balanced-binary-tree.ts
@@ -0,0 +1,14 @@
+function isBalanced(root: TreeNode | null): boolean {
+  function dfs_getHeight(root: TreeNode | null): [boolean, number] {
+    if (!root) return [true, 0];
+
+    const left = dfs_getHeight(root.left);
+    const right = dfs_getHeight(root.right);
+
+    const balance = left[0] && right[0] && Math.abs(left[1] - right[1]) <= 1;
+
+    return [balance, 1 + Math.max(left[1], right[1])];
+  }
+
+  return dfs_getHeight(root)[0];
+}
