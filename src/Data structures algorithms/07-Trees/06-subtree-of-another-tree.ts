ee0fc61855c8cbdac4d2394e0684d9ffd55aa150
Vipul Dessai <badgamerbad@gmail.com>
2022-05-23 01:56:20 +0530

    dsa

---
+++ src/Data structures algorithms/07-Trees/06-subtree-of-another-tree.ts
@@ -0,0 +1,16 @@
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+  if(!subRoot) return true;
+  if(!root) return false;
+
+  if(sameTree(root, subRoot)) return true;
+
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+
+  function sameTree(root: TreeNode | null, subRoot: TreeNode | null) {
+    if(!root && !subRoot) return true;
+
+    if(root && subRoot && root.val === subRoot.val) {
+      return sameTree(root.left, subRoot.left) && sameTree(root.right, subRoot.right)
+    }
+  }
+};
