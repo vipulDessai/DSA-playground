2e854fa91e2646502adb1f6e9d6d33aff567b5e0
Vipul Dessai <badgamerbad@gmail.com>
2022-05-29 23:10:25 +0530

    dsa

--- src/random-leet-code-problems/002-positive.js
+++ src/random-leet-code-problems/002-positive.js
@@ -1,5 +1,14 @@
-// return a positive number in an array not present in an array
-// refer - https://www.geeksforgeeks.org/find-the-smallest-positive-number-missing-from-an-unsorted-array/
+/**
+ * This is a demo task.
+ * Write a function: function solution(A);
+ * that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+ * For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+ * Given A = [1, 2, 3], the function should return 4.
+ * Given A = [−1, −3], the function should return 1.
+ * Write an efficient algorithm for the following assumptions:
+ * N is an integer within the range [1..100,000];
+ * each element of array A is an integer within the range [−1,000,000..1,000,000].
+ */
 
 function solution(array) {
   const unsortedSet = new Set();
@@ -13,18 +22,22 @@ function solution(array) {
 
   const sortedArray = [...unsortedSet].sort((a, b) => a - b);
 
-  sortedArray.push(sortedArray[sortedArray.length - 1] + 2);
+  if (sortedArray[0] === 1) {
+    sortedArray.push(sortedArray[sortedArray.length - 1] + 2);
 
-  for (let i = 0; i < sortedArray.length - 1; i++) {
-    // skip
-    if (sortedArray[i] < 0) {
-      continue;
-    }
+    for (let i = 0; i < sortedArray.length - 1; i++) {
+      // skip
+      if (sortedArray[i] < 0) {
+        continue;
+      }
 
-    const subtractionResult = sortedArray[i + 1] - sortedArray[i];
-    if (subtractionResult > 1) {
-      return sortedArray[i] + 1;
+      const subtractionResult = sortedArray[i + 1] - sortedArray[i];
+      if (subtractionResult > 1) {
+        return sortedArray[i] + 1;
+      }
     }
+  } else {
+    return 1;
   }
 }
 
