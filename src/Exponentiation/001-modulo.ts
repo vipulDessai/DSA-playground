88533f182ae0fde414590262285c34f54c2f8a80
Vipul Dessai <badgamerbad@gmail.com>
2022-06-03 00:40:29 +0530

    dsa

--- src/Exponentiation/001-modulo.ts
+++ src/Exponentiation/001-modulo.ts
@@ -8,27 +8,31 @@ function factorial() {
     ans *= base % MOD;
     largeOut.push(ans);
   }
+
+  return largeOut;
 }
 
 // console.log(factorial());
 
+// const MOD = 1000000007;
+const MOD = 10;
 function fastExp(base, exp) {
   if (exp == 1) {
     return base;
   } else {
     if (exp % 2 == 0) {
       const base1 = Math.pow(fastExp(base, exp / 2), 2);
-      if (base1 >= 1000000007) return base1 % 1000000007;
+      if (base1 >= MOD) return base1 % MOD;
       else return base1;
     } else {
       const ans = base * Math.pow(fastExp(base, (exp - 1) / 2), 2);
-      if (ans >= 1000000007) return ans % 1000000007;
+      if (ans >= MOD) return ans % MOD;
       else return ans;
     }
   }
 }
 
-// console.log(fastExp(2, 1000));
+console.log(fastExp(2, 14));
 
 let num = 2004;
 const arr: number[] = [];
@@ -36,4 +40,4 @@ while (num > 0) {
   arr.push(num % 10);
   num = (num - (num % 10)) / 10;
 }
-console.log(arr);
+// console.log(arr);
