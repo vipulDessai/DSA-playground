4175fbfa2cef9c8372de8ce5e4c9a179d797cda1
Vipul Dessai <badgamerbad@gmail.com>
2022-10-29 10:05:00 +0530

    segment tree

---
+++ src/Data structures algorithms/26-segment-tree/001-segment-tree.ts
@@ -0,0 +1,26 @@
+function constructTree(
+  input: number[],
+  segTree: number[],
+  l: number,
+  h: number,
+  pos: number,
+) {
+  if (l === h) {
+    return (segTree[pos] = input[l]);
+  }
+
+  const m = (l + h - ((l + h) % 2)) / 2;
+
+  constructTree(input, segTree, l, m, 2 * pos + 1);
+  constructTree(input, segTree, m, h, 2 * pos + 2);
+
+  return (segTree[pos] = Math.min(segTree[2 * pos + 1], segTree[2 * pos + 2]));
+}
+
+const input = [-1, 2, 4, 0];
+const segTree: number[] = [];
+for (let i = 0; i < 4 * 2 - 1; i++) {
+  segTree.push(Infinity);
+}
+
+console.log(constructTree(input, segTree, 0, 3, 0));
