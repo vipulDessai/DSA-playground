bd9cce74778a2d6a5e1f6343b75e547b60e37875
Vipul Dessai <badgamerbad@gmail.com>
2023-02-11 02:41:04 +0530

    dsa notes

--- src/Data structures algorithms/26-segment-tree/001-segment-tree.ts
+++ src/Data structures algorithms/26-segment-tree/001-segment-tree.ts
@@ -5,14 +5,14 @@ function constructTree(
   h: number,
   pos: number,
 ) {
-  if (l === h) {
+   if (l === h) {
     return (segTree[pos] = input[l]);
   }
 
   const m = (l + h - ((l + h) % 2)) / 2;
 
   constructTree(input, segTree, l, m, 2 * pos + 1);
-  constructTree(input, segTree, m, h, 2 * pos + 2);
+  constructTree(input, segTree, m + 1, h, 2 * pos + 2);
 
   return (segTree[pos] = Math.min(segTree[2 * pos + 1], segTree[2 * pos + 2]));
 }
@@ -23,4 +23,6 @@ for (let i = 0; i < 4 * 2 - 1; i++) {
   segTree.push(Infinity);
 }
 
-console.log(constructTree(input, segTree, 0, 3, 0));
+constructTree(input, segTree, 0, 3, 0)
+
+console.log(segTree);
