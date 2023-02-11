6a09caf0038ff41c59cbda28479b3d01f1c27a84
Vipul Dessai <badgamerbad@gmail.com>
2023-02-12 01:35:04 +0530

    dsa notes

--- src/Data structures algorithms/26-segment-tree/001-segment-tree.ts
+++ src/Data structures algorithms/26-segment-tree/001-segment-tree.ts
@@ -31,3 +31,29 @@ for (let i = 0; i < 4 * 2 - 1; i++) {
 constructTree(input, segTree, 0, 3, 0);
 
 console.log(segTree);
+
+function rangeMinQuery(
+  segTree: number[],
+  qLow: number,
+  qHigh: number,
+  l: number,
+  h: number,
+  pos: number,
+) {
+  if (qLow <= l && qHigh >= h) {
+    return segTree[pos];
+  }
+
+  if (qLow > h || qHigh < l) {
+    return Infinity;
+  }
+
+  const m = (l + h - (l + (h % 2))) / 2;
+
+  return Math.min(
+    rangeMinQuery(segTree, qLow, qHigh, l, m, 2 * pos + 1),
+    rangeMinQuery(segTree, qLow, qHigh, m + 1, h, 2 * pos + 2),
+  );
+}
+
+console.log(rangeMinQuery(segTree, 2, 3, 0, 3, 0));
