b8c437e5553d7b9dbf601377596363ec606683d1
Vipul Dessai <badgamerbad@gmail.com>
2024-02-25 18:12:18 +0530

    dsa

---
+++ src/Data structures algorithms/09-Heap-or-Priority-Queue/002-min-heap.ts
@@ -0,0 +1,29 @@
+class MinHeap_O_N {
+  arr: number[];
+  constructor() {
+    this.arr = [];
+  }
+
+  push(n: number) {
+    for (let i = 0; i < this.arr.length; ++i) {
+      if (n < this.arr[i]) {
+        [this.arr[i], n] = [n, this.arr[i]];
+      }
+    }
+
+    this.arr.push(n);
+  }
+}
+
+const mH_o_n = new MinHeap_O_N();
+mH_o_n.push(1);
+mH_o_n.push(9);
+mH_o_n.push(16);
+mH_o_n.push(25);
+mH_o_n.push(0);
+mH_o_n.push(6);
+mH_o_n.push(18);
+
+console.log(mH_o_n.arr);
+
+class MinHeap_Log_N {}
