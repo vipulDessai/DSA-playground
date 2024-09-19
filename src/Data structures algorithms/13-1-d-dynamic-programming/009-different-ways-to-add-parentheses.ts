9cb92d975b93115a9fe98758f200588aefafe9c9
Vipul Dessai <badgamerbad@gmail.com>
2024-09-20 03:10:37 +0530

    dsa

---
+++ src/Data structures algorithms/13-1-d-dynamic-programming/009-different-ways-to-add-parentheses.ts
@@ -0,0 +1,30 @@
+function diffWaysToCompute(expression: string): number[] {
+  let curNum = '';
+  const expStrArr: (string | number)[] = [];
+  for (let i = 0; i < expression.length; ++i) {
+    if (
+      expression[i] === '+' ||
+      expression[i] === '-' ||
+      expression[i] === '*'
+    ) {
+      expStrArr.push(parseInt(curNum));
+      expStrArr.push(expression[i]);
+      curNum = '';
+    } else {
+      curNum = curNum.concat(expression[i]);
+    }
+  }
+
+  expStrArr.push(parseInt(curNum));
+
+  const res = [];
+  const dfs = () => {};
+
+  dfs();
+
+  return res;
+}
+
+var inStr = '2*3-4*5';
+var inStr = '2*33-4*59';
+console.log(diffWaysToCompute(inStr));
