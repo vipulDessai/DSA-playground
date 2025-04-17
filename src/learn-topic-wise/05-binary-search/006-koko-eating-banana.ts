4f95d70025edaea6a43c0bd5fc059df2a7605ab9
Vipul Dessai <badgamerbad@gmail.com>
2025-04-17 23:53:19 +0530

    dsa

---
+++ src/learn-topic-wise/05-binary-search/006-koko-eating-banana.ts
@@ -0,0 +1,33 @@
+function minEatingSpeed(piles: number[], h: number): number {
+  function feasible(capacity: number) {
+    let i = 0,
+      j = 0;
+    while (i < h && j < piles.length) {
+      i += Math.ceil(piles[j] / capacity);
+      ++j;
+    }
+
+    if (i <= h && j == piles.length) {
+      return true;
+    }
+
+    return false;
+  }
+
+  let l = 1;
+  let r = Math.max(...piles);
+
+  while (l < r) {
+    const m = Math.floor(l + (r - l) / 2);
+
+    if (feasible(m)) {
+      r = m;
+    } else {
+      l = m + 1;
+    }
+  }
+
+  return l;
+}
+
+minEatingSpeed([3, 6, 7, 11], 8);
