b8b908cdfc575da5de088a40f63cac17fee74950
Vipul Dessai <badgamerbad@gmail.com>
2025-05-01 22:24:26 +0530

    notes

--- src/learn-topic-wise/04-binary-search/001-binary-search.ts
+++ src/learn-topic-wise/04-binary-search/001-binary-search.ts
@@ -17,3 +17,7 @@ function searchInsert(nums: number[], target: number): number {
 
   return l;
 }
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
