2c623fbb6a577022ef78c9fbef0133e4e9cf02f7
Vipul Dessai <badgamerbad@gmail.com>
2025-05-21 00:54:35 +0530

    dsa

---
+++ src/learn-topic-wise/01-hash/010-longest-substring-without-repeated-chars.ts
@@ -0,0 +1,6 @@
+export const foo = 'bar';
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+function lengthOfLongestSubstring(s: string): number {}
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
