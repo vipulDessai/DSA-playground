65e5c24864d65c287928366cded5f4808a63c06a
Vipul Dessai <badgamerbad@gmail.com>
2025-06-06 13:24:04 +0530

    dsa

---
+++ src/learn-topic-wise/01-hash/013-reconstruct.itinerary.ts
@@ -0,0 +1,46 @@
+function findItinerary(tickets: string[][]): string[] {
+  const adjList = new Map<string, string[]>();
+
+  for (let i = 0; i < tickets.length; ++i) {
+    const [dep, arr] = tickets[i];
+
+    if (!adjList.has(dep)) {
+      adjList.set(dep, []);
+    }
+
+    adjList.get(dep)!.push(arr);
+  }
+
+  for (const cur of adjList) {
+    const [key, val] = cur;
+    val.sort((a, b) => a.localeCompare(b));
+  }
+
+  const res: string[] = [];
+
+  function dfs(n: string) {
+    const childNodes = adjList.get(n);
+
+    if (childNodes) {
+      while (childNodes.length) {
+        dfs(childNodes.shift()!);
+      }
+    }
+
+    res.unshift(n);
+  }
+
+  dfs('JFK');
+
+  return res;
+}
+
+console.log(
+  findItinerary([
+    ['JFK', 'SFO'],
+    ['JFK', 'ATL'],
+    ['SFO', 'ATL'],
+    ['ATL', 'JFK'],
+    ['ATL', 'SFO'],
+  ]),
+);
