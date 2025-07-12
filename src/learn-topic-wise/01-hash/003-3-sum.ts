050bbbd57450429ffebf69ea1ab68d5caf910555
Vipul Dessai <badgamerbad@gmail.com>
2025-07-12 15:13:22 +0530

    dsa

--- src/learn-topic-wise/01-hash/003-3-sum.ts
+++ src/learn-topic-wise/01-hash/003-3-sum.ts
@@ -1,6 +1,8 @@
-export function threeSum(nums: number[]): number[][] {
+export const url = '[3Sum](https://leetcode.com/problems/3sum/description/)';
+
+function threeSum(nums: number[]): number[][] {
   const n = nums.length;
-  nums.sort((a, b) => a - b); // Sorting helps with efficient searching
+  nums.sort((a, b) => a - b); // Sorting helps with efficient searching, a - b sorts in asc order
   const result = new Set<string>(); // Using a Set to store unique triplets
 
   for (let i = 0; i < n - 2; i++) {
@@ -25,4 +27,6 @@ export function threeSum(nums: number[]): number[][] {
   return Array.from(result).map((triplet) => JSON.parse(triplet));
 }
 
-console.log(threeSum([-1, 0, 1, 2, -1, -4])); // Output: [[-1, -1, 2], [-1, 0, 1]]
+var input = [-1, 0, 1, 2, -1, -4];
+var out = threeSum(input);
+console.log(out); // Output: [[-1, -1, 2], [-1, 0, 1]]
--- src/learn-topic-wise/01-hash/003-4-sum-i.ts
+++ src/learn-topic-wise/01-hash/003-4-sum-i.ts
@@ -1,24 +1,20 @@
-function threeSum(
-  nums: number[],
-  start: number,
-  target: number,
-  res: Set<string>,
-) {
+export const url = '[4Sum](https://leetcode.com/problems/4sum/description/)';
+
+function threeSum(nums: number[], i: number, t: number, res: Set<string>) {
   const n = nums.length;
 
-  for (let j = start + 1; j < nums.length; ++j) {
+  for (let j = i + 1; j < n - 2; ++j) {
     let l = j + 1;
     let r = n - 1;
 
     while (l < r) {
-      const sum = nums[start] + nums[j] + nums[l] + nums[r];
-
-      if (sum < target) {
+      const sum = nums[i] + nums[j] + nums[l] + nums[r];
+      if (sum < t) {
         ++l;
-      } else if (sum > target) {
+      } else if (sum > t) {
         --r;
       } else {
-        res.add(JSON.stringify([nums[start], nums[j], nums[l], nums[r]]));
+        res.add(JSON.stringify([nums[i], nums[j], nums[l], nums[r]]));
         ++l;
         --r;
       }
@@ -29,12 +25,18 @@ function threeSum(
 function fourSum(nums: number[], target: number): number[][] {
   const n = nums.length;
 
-  nums.sort((a, b) => a - b);
+  nums.sort((a, b) => a - b); // asc order sort
 
   const res = new Set<string>();
-  for (let i = 0; i < n; ++i) {
+  for (let i = 0; i < n - 3; ++i) {
     threeSum(nums, i, target, res);
   }
 
   return Array.from(res).map((r) => JSON.parse(r));
 }
+
+var input = [0, 0, 0, 0],
+  target = 0;
+var out = fourSum(input, target);
+
+console.log(out);
