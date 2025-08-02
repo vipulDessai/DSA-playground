16ad75c318e43123cbf74b34723aaff561978569
Vipul Dessai <badgamerbad@gmail.com>
2025-08-02 19:52:54 +0530

    dsa

--- src/learn-topic-wise/04-two-pointers/002-3-sum-closest.ts
+++ src/learn-topic-wise/04-two-pointers/002-3-sum-closest.ts
@@ -2,9 +2,36 @@ export const url =
   '[3 Sum Closest](https://leetcode.com/problems/3sum-closest/description/)';
 
 function threeSumClosest(nums: number[], target: number): number {
-  return 0;
+  const n = nums.length;
+
+  nums.sort((a, b) => a - b);
+
+  let res = Infinity;
+  for (let i = 0; i < n - 2; ++i) {
+    let l = i + 1;
+    let r = n - 1;
+
+    let sum = 0;
+    while (l < r) {
+      sum = nums[i] + nums[l] + nums[r];
+
+      if (Math.abs(target - sum) < Math.abs(target - res)) {
+        res = sum;
+      }
+
+      if (sum < target) {
+        ++l;
+      } else {
+        --r;
+      }
+    }
+  }
+
+  return res;
 }
 
 var input = [-1, 2, 1, -4],
   t = 1;
 var out = threeSumClosest(input, t);
+
+console.log(out);
