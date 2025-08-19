d58ebfc2fdc45936f03ee502b552fc5cab0f80a4
Vipul Dessai <badgamerbad@gmail.com>
2025-08-19 16:16:00 +0530

    dsa

--- src/learn-topic-wise/04-two-pointers/007-intersection-of-two-arrays.ts
+++ src/learn-topic-wise/04-two-pointers/007-intersection-of-two-arrays.ts
@@ -5,26 +5,25 @@ function intersection(nums1: number[], nums2: number[]): number[] {
   nums1.sort((a, b) => a - b);
   nums2.sort((a, b) => a - b);
 
-  const result: number[] = [];
   let i = 0,
-    j = 0;
+    j = 0,
+    res = new Set<number>();
 
   while (i < nums1.length && j < nums2.length) {
     if (nums1[i] === nums2[j]) {
-      // Avoid duplicates in result
-      if (result.length === 0 || result[result.length - 1] !== nums1[i]) {
-        result.push(nums1[i]);
-      }
-      i++;
-      j++;
-    } else if (nums1[i] < nums2[j]) {
-      i++;
+      res.add(nums1[i]);
+      ++i;
+      ++j;
     } else {
-      j++;
+      if (nums1[i] < nums2[j]) {
+        ++i;
+      } else {
+        ++j;
+      }
     }
   }
 
-  return result;
+  return Array.from(res);
 }
 
 var i1 = [4, 9, 5],
